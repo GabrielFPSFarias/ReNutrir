58a069ee200c504050fc043ecff26c1831b34d06
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:53:41 -0300

    Update Instituicao.java

--- src/br/com/renutrir/model/Instituicao.java
+++ src/br/com/renutrir/model/Instituicao.java
@@ -1,1 +1,11 @@
+package br.com.renutrir.model;
+
+public class Instituicao extends Usuario {
+    private String cnpj;
+    private String endereco;
+    private boolean necessidadeAtiva;
+
+    // Construtor, getters e setters
+
+}
 
