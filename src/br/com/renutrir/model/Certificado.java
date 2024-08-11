89854620381581c6acbb4e96994b714c0df976c2
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:55:42 -0300

    Update Certificado.java

--- src/br/com/renutrir/model/Certificado.java
+++ src/br/com/renutrir/model/Certificado.java
@@ -1,1 +1,12 @@
+package br.com.renutrir.model;
+
+import java.time.LocalDate;
+
+public class Certificado {
+    private Doador doador;
+    private String descricao;
+    private LocalDate dataEmissao;
+
+    // Construtor, getters e setters
+}
 
