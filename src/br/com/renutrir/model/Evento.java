6c8fe6124757542df5c34eefe9b60e4ed78dbd0b
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:54:09 -0300

    Update Evento.java

--- src/br/com/renutrir/model/Evento.java
+++ src/br/com/renutrir/model/Evento.java
@@ -1,1 +1,14 @@
+package br.com.renutrir.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Evento {
+    private String nome;
+    private LocalDate data;
+    private String local;
+    private List<Doacao> doacoes;
+
+    // Construtor, getters e setters
+}
 
