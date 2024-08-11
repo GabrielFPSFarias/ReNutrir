03f163a7aa1905218b2857c0bea63b4e2940c87e
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:42:49 -0300

    Criado main.java

---
+++ Código/src/br/com/renutrir/main/main.java
@@ -0,0 +1,13 @@
+package br.com.renutrir.main;
+
+import br.com.renutrir.model.Doador;
+import br.com.renutrir.model.Instituicao;
+import br.com.renutrir.service.CadastroDoadorService;
+import br.com.renutrir.service.CadastroInstituicaoService;
+
+public class Main {
+    public static void main(String[] args) {
+
+    	
+    }
+}
