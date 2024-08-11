11c2d13af6b38dbc669d3efd9159429c40f67e3a
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:54:56 -0300

    Update Doador.java

--- src/br/com/renutrir/model/Doador.java
+++ src/br/com/renutrir/model/Doador.java
@@ -1,1 +1,13 @@
+package br.com.renutrir.model;
+
+public class Doador extends Usuario {
+    private int nivel;
+
+    // Construtor, getters e setters
+
+    public void doar(Doacao doacao) {
+        // Implementação do método de doação
+    }
+
+}
 
