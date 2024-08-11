2b0482997650c07c42a9254f0c7294270ad78fc4
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:56:24 -0300

    Update main.java

--- src/br/com/renutrir/main/main.java
+++ src/br/com/renutrir/main/main.java
@@ -1,9 +1,10 @@
 package br.com.renutrir.main;
 
 import br.com.renutrir.model.Doador;
+import br.com.renutrir.model.Doacao;
 import br.com.renutrir.model.Instituicao;
-import br.com.renutrir.service.CadastroDoadorService;
-import br.com.renutrir.service.CadastroInstituicaoService;
+import br.com.renutrir.service.CadastroDoador;
+import br.com.renutrir.service.CadastroInstituicao;
 
 public class Main {
     public static void main(String[] args) {
