8ca3dca46be65e016db921f1b9b5ec1cea230ed9
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:58:04 -0300

    Update CadastroInstituicao.java

--- src/br/com/renutrir/service/CadastroInstituicao.java
+++ src/br/com/renutrir/service/CadastroInstituicao.java
@@ -1,1 +1,9 @@
+package br.com.renutrir.service;
 
+import br.com.renutrir.model.Instituicao;
+
+public class CadastroInstituicaoService {
+    public void cadastrar(Instituicao instituicao) {
+    
+    }
+}
