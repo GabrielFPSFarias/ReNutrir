dc37c42150ded0bc861df47d500b44ce1146174d
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:55:23 -0300

    Update Doacao.java

--- src/br/com/renutrir/model/Doacao.java
+++ src/br/com/renutrir/model/Doacao.java
@@ -1,1 +1,14 @@
+package br.com.renutrir.model;
+
+import java.time.LocalDate;
+
+public class Doacao {
+    private String tipo;
+    private String descricao;
+    private Doador doador;
+    private Instituicao instituicao;
+    private LocalDate data;
+
+    // Construtor, getters e setters
+}
 
