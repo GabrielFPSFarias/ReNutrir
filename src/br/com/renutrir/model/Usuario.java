a5dcb7dab48aaf47a16cf8b754a64d8756649808
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:52:59 -0300

    Update Usuario.java

--- src/br/com/renutrir/model/Usuario.java
+++ src/br/com/renutrir/model/Usuario.java
@@ -1,1 +1,10 @@
+package br.com.renutrir.model;
 
+public abstract class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    // Construtor, getters e setters
+}
