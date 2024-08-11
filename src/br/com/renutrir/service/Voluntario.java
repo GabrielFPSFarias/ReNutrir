524a1c831f33b7e4d44286a5ebba98bdf542cacd
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-11 17:58:28 -0300

    Update Voluntario.java

--- src/br/com/renutrir/service/Voluntario.java
+++ src/br/com/renutrir/service/Voluntario.java
@@ -1,1 +1,11 @@
+package br.com.renutrir.service;
+
+import br.com.renutrir.model.Doador;
+import br.com.renutrir.model.Instituicao;
+
+public class VoluntariadoService {
+    public void registrarVoluntario(Doador doador, Instituicao instituicao) {
+        // Implementação do método para registrar voluntário
+    }
+}
 
