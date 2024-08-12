658562ad695a8146d78c0ae1d132fe037a64b25f
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-12 10:35:24 -0300

    Update Usuario.java

--- src/br/com/renutrir/model/Usuario.java
+++ src/br/com/renutrir/model/Usuario.java
@@ -1,10 +1,37 @@
 package br.com.renutrir.model;
 
-public abstract class Usuario {
-    private String nome;
-    private String email;
-    private String senha;
-    private String telefone;
+import br.com.renutrir.servicos.Cadastro;
 
-    // Construtor, getters e setters
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Usuario extends Cadastro {
+    private LocalDate dataNascimento;
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimentoStr) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate dataNascimento = LocalDate.parse(dataNascimentoStr, formatter);
+            if (dataNascimento.isAfter(LocalDate.now())) {
+                throw new IllegalArgumentException("A data de nascimento não pode ser no futuro.");
+            }
+            this.dataNascimento = dataNascimento;
+        } catch (DateTimeParseException e) {
+            System.out.println("Formato de data inválido. Use o formato dia/mês/ano.");
+        }
+    }
+
+    public int calcularIdade() {
+        if (dataNascimento == null) {
+            throw new IllegalStateException("Data de nascimento não foi definida.");
+        }
+        LocalDate dataAtual = LocalDate.now();
+        return Period.between(dataNascimento, dataAtual).getYears();
+    }
 }
