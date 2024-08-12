bb6e4f914893ad56e8758fdc3bea1d1fd8e1a175
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-12 10:36:42 -0300

    Delete src/br/com/renutrir/service directory

--- src/br/com/renutrir/service/CadastroInstituicao.java
+++
@@ -1,9 +0,0 @@
-package br.com.renutrir.service;
-
-import br.com.renutrir.model.Instituicao;
-
-public class CadastroInstituicaoService {
-    public void cadastrar(Instituicao instituicao) {
-    
-    }
-}
