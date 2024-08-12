3cc165f84de58c375d6c31674d0aebce2b2ac03c
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-12 10:40:04 -0300

    Update Instituicao.java

--- src/br/com/renutrir/model/Instituicao.java
+++ src/br/com/renutrir/model/Instituicao.java
@@ -1,11 +1,49 @@
 package br.com.renutrir.model;
 
-public class Instituicao extends Usuario {
+import br.com.renutrir.servicos.Cadastro;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Instituicao extends Cadastro {
     private String cnpj;
-    private String endereco;
-    private boolean necessidadeAtiva;
+    private LocalDate dataFundacao;
 
-    // Construtor, getters e setters
+    public LocalDate getDataFundacao() {
+        return dataFundacao;
+    }
 
+    public void setDataFundacao(String dataFundacaoStr) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate dataFundacao = LocalDate.parse(dataFundacaoStr, formatter);
+            if (dataFundacao.isAfter(LocalDate.now())) {
+                throw new IllegalArgumentException("A data de fundação não pode ser no futuro.");
+            }
+            this.dataFundacao = dataFundacao;
+        } catch (DateTimeParseException e) {
+            System.out.println("Formato de data inválido. Use o formato dd/MM/yyyy.");
+        }
+    }
+
+    public int calcularFundacao() {
+        if (dataFundacao == null) {
+            throw new IllegalStateException("Data de fundação não foi definida.");
+        }
+        LocalDate dataAtual = LocalDate.now();
+        return Period.between(dataFundacao, dataAtual).getYears();
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+        //Falta criar lógica
+    }
 }
+
 
