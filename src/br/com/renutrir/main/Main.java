dd86c74f436249009b2cdb07b1ed326e59459df0
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 17:56:18 -0300

    Update Main.java

--- src/br/com/renutrir/main/Main.java
+++ src/br/com/renutrir/main/Main.java
@@ -1,13 +1,31 @@
 package br.com.renutrir.main;
 
-import br.com.renutrir.model.Doador;
-import br.com.renutrir.model.Doacao;
-import br.com.renutrir.model.Instituicao;
-import br.com.renutrir.servicos.Cadastro;
+import br.com.renutrir.repositorio.*;
+import br.com.renutrir.servicos.*;
+import br.com.renutrir.model.*;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-    	
+       Doador d = new Doador();
+       Instituicao i = new Instituicao();
+
+    	d.setEmail("doador@email.com");
+    	i.setEmail("inst@email.com");
+    	d.setNome("Doador");
+    	i.setNome("Inst");
+    	d.setNomeUsuario("Doador01");
+    	i.setNomeUsuario("Inst01");
+    	d.setSenha("password");
+    	i.setSenha("senha");
+    	d.setTelefone("12345678");
+    	i.setTelefone("87654321");
+
+    	Certificado c = new Certificado();
+    	c.setDescricao("Doador bacana");
+    	c.setDataEmissao(LocalDate.of(2020, 12, 10));
+    	d.setCertificado(c);
+    	d.setDataNascimento(LocalDate.of(2000, 8, 1));
     }
 }
