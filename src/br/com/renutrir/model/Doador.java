93c05c3d6c821eaba387a60d5a7c405ccfc3943e
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 14:53:39 -0300

    Update Doador.java

--- src/br/com/renutrir/model/Doador.java
+++ src/br/com/renutrir/model/Doador.java
@@ -2,6 +2,7 @@ package br.com.renutrir.model;
 
 public class Doador extends Usuario {
     private int nivel;
+    private Certificado certificado;
 
     // Construtor, getters e setters
 
@@ -9,5 +10,19 @@ public class Doador extends Usuario {
         // Implementação do método de doação
     }
 
-}
+    public void setNivel(int nivel){
+        this.nivel = nivel;
+    }
 
+    public void setCertificado(Certificado certificado){
+        this.certificado = certificado;
+    }
+
+    public int getNivel(){
+        return this.nivel;
+    }
+
+    public Certificado getCertificado(){
+        return this.certificado;
+    }
+}
