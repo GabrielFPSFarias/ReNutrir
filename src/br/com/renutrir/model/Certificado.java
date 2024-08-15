f00c24b0f1d7fc754dbd860e08880a698328ace9
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 14:47:09 -0300

    Update Certificado.java

--- src/br/com/renutrir/model/Certificado.java
+++ src/br/com/renutrir/model/Certificado.java
@@ -3,10 +3,22 @@ package br.com.renutrir.model;
 import java.time.LocalDate;
 
 public class Certificado {
-    private Doador doador;
     private String descricao;
     private LocalDate dataEmissao;
 
-    // Construtor, getters e setters
-}
+    public String getDescricao() {
+        return descricao;
+    }
 
+     public LocalDate getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setDataEmissao(LocalDate dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+}
