f75954a36bf89c06757f4cb35348332bc0db3c1d
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 15:49:03 -0300

    Update Evento.java

--- src/br/com/renutrir/model/Evento.java
+++ src/br/com/renutrir/model/Evento.java
@@ -9,6 +9,35 @@ public class Evento {
     private String local;
     private List<Doacao> doacoes;
 
-    // Construtor, getters e setters
-}
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public List<Doacao> getDoacoes() {
+        return doacoes;
+    }
+
+    public void setDoacoes(List<Doacao> doacoes) {
+        this.doacoes = doacoes;
+    }
+}
