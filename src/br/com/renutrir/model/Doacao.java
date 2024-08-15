0e091c8c843707b5753244f5843aafb88b36e081
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 15:42:14 -0300

    Update Doacao.java

--- src/br/com/renutrir/model/Doacao.java
+++ src/br/com/renutrir/model/Doacao.java
@@ -4,11 +4,55 @@ import java.time.LocalDate;
 
 public class Doacao {
     private String tipo;
-    private String descricao;
     private Doador doador;
     private Instituicao instituicao;
     private LocalDate data;
 
-    // Construtor, getters e setters
-}
+    private int qtd;
 
+    public Doacao(String tipo, Doador doador, int qtd, Instituicao instituicao){
+    setTipo(tipo);
+    setDoador(doador);
+    setQtd(qtd);
+    setInstituicao(instituicao);
+    }
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Doador getDoador() {
+        return doador;
+    }
+
+    public void setDoador(Doador doador) {
+        this.doador = doador;
+    }
+
+    public Instituicao getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(Instituicao instituicao) {
+        this.instituicao = instituicao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
