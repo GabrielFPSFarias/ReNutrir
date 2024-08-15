1765c71aa7ff8b70cbdea5fbe974d78abc608abc
GabrielFPSFarias <168372750+GabrielFPSFarias@users.noreply.github.com>
2024-08-15 15:17:30 -0300

    Update Doador.java

--- src/br/com/renutrir/model/Doador.java
+++ src/br/com/renutrir/model/Doador.java
@@ -1,7 +1,13 @@
 package br.com.renutrir.model;
 
-public class Doador extends Usuario {
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Doador extends Cadastro{
     private int nivel;
+    private LocalDate dataNascimento;
     private Certificado certificado;
 
     // Construtor, getters e setters
@@ -18,11 +24,31 @@ public class Doador extends Usuario {
         this.certificado = certificado;
     }
 
+    public void setDataNascimento(String dataNascimentoStr) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate dataNascimento = LocalDate.parse(dataNascimentoStr, formatter);
+            if (dataNascimento.isAfter(LocalDate.now())) {
+                throw new IllegalArgumentException("A data de nascimento não pode ser no futuro.");
+            }
+            this.dataNascimento = dataNascimento;
+        } catch (DateTimeParseException e) {
+            System.out.println("Formato de data inválido. Use o formato dia/mês/ano.");
+        }
+    }
+
     public int getNivel(){
         return this.nivel;
     }
 
-    public Certificado getCertificado(){
+    public Certificado getCertificado() {
         return this.certificado;
     }
+        public int calcularIdade() {
+            if (dataNascimento == null) {
+                throw new IllegalStateException("Data de nascimento não foi definida.");
+            }
+            LocalDate dataAtual = LocalDate.now();
+            return Period.between(dataNascimento, dataAtual).getYears();
+        }
 }
