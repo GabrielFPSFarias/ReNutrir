1831cc99668e71b90ca3ea469f2dc7030036c703
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-17 12:36:56 -0300

    Update Doador.java

--- src/br/com/renutrir/model/Doador.java
+++ src/br/com/renutrir/model/Doador.java
@@ -11,6 +11,7 @@ public class Doador extends Conta {
     private int nivel;
     private LocalDate dataNascimento;
     private Certificado certificado;
+    private String cpf;
 
     // Construtor, getters e setters
 
@@ -41,11 +42,49 @@ public class Doador extends Conta {
     public Certificado getCertificado() {
         return this.certificado;
     }
-        public int calcularIdade() {
-            if (dataNascimento == null) {
-                throw new IllegalStateException("Data de nascimento não foi definida.");
-            }
-            LocalDate dataAtual = LocalDate.now();
-            return Period.between(dataNascimento, dataAtual).getYears();
+    public int calcularIdade() {
+        if (dataNascimento == null) {
+            throw new IllegalStateException("Data de nascimento não foi definida.");
         }
+        LocalDate dataAtual = LocalDate.now();
+        return Period.between(dataNascimento, dataAtual).getYears();
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (!CpfValidacao(cpf)) {
+            throw new IllegalArgumentException("CPF inválido. Deve conter 11 dígitos.");
+        }
+        this.cpf = cpf;
+    }
+
+    private boolean CpfValidacao(String cpf) {
+        if (cpf == null || !cpf.matches("\\d{11}")) {
+            return false;
+        }
+
+        char[] cpfArray = cpf.toCharArray();
+        int[] cpfInt = new int[11];
+        for (int i = 0; i < 11; i++) {
+            cpfInt[i] = cpfArray[i] - '0';
+        }
+
+        int soma1 = 0, soma2 = 0;
+        for (int i = 0; i < 9; i++) {
+            soma1 += cpfInt[i] * (10 - i);
+            soma2 += cpfInt[i] * (11 - i);
+        }
+
+        int resto1 = soma1 % 11;
+        int digito1 = (resto1 < 2) ? 0 : 11 - resto1;
+
+        soma2 += digito1 * 2;
+        int resto2 = soma2 % 11;
+        int digito2 = (resto2 < 2) ? 0 : 11 - resto2;
+
+        return (digito1 == cpfInt[9] && digito2 == cpfInt[10]);
+    }
 }
