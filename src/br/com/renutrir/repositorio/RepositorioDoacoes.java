df5a93c3e36c0e5f22579d5fd6c4030e58042213
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-17 11:59:48 -0300

    Update RepositorioDoacoes.java

--- src/br/com/renutrir/repositorio/RepositorioDoacoes.java
+++ src/br/com/renutrir/repositorio/RepositorioDoacoes.java
@@ -1,6 +1,28 @@
 package br.com.renutrir.repositorio;
 
+import br.com.renutrir.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepositorioDoacoes {
 
-}
+    private List<Doacao> doacoes;
 
+    public RepositorioDoacoes() {
+        this.doacoes = new ArrayList<>();
+    }
+
+    public void adicionarDoacao(Doacao doacao) {
+        doacoes.add(doacao);
+    }
+
+    public void removerDoacao(Doacao doacao) {
+        doacoes.remove(doacao);
+    }
+
+    //Listar todas as doações feitas
+    public List<Doacao> listarDoacoes() {
+        return new ArrayList<>(doacoes);
+    }
+}
