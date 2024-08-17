79e05644add374b651c3faee70f15836ded3e456
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-17 12:38:27 -0300

    Update RepositorioCertificado.java

--- src/br/com/renutrir/repositorio/RepositorioCertificado.java
+++ src/br/com/renutrir/repositorio/RepositorioCertificado.java
@@ -1,7 +1,24 @@
 package br.com.renutrir.repositorio;
 
+import br.com.renutrir.model.Certificado;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepositorioCertificado {
 
-  // D
-}
+    private List<Certificado> certificados;
 
+    public RepositorioCertificado() {
+        this.certificados = new ArrayList<>();
+    }
+
+    public void adicionarCertificado(Certificado certificado) {
+        certificados.add(certificado);
+    }
+
+    public List<Certificado> listarCertificados() {
+        return new ArrayList<>(certificados);
+    }
+
+
+}
