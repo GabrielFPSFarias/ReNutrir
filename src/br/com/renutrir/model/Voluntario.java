37274177af4ce51ddaa8977b71c1e08d3a4e2d10
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-18 11:52:29 -0300

    Update Voluntario.java

--- src/br/com/renutrir/model/Voluntario.java
+++ src/br/com/renutrir/model/Voluntario.java
@@ -1,5 +1,22 @@
 package br.com.renutrir.model;
 
 public class Voluntario extends Doador{
+    private String disponibilidade; // Ex: dias e horários disponíveis
+    private String funcaoVoluntariado; // o cargo que o voluntário vai cumprir
 
+    public String getDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(String disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+    public String getFuncaoVoluntariado() {
+        return funcaoVoluntariado;
+    }
+
+    public void setFuncaoVoluntariado(String funcaoVoluntariado) {
+        this.funcaoVoluntariado = funcaoVoluntariado;
+    }
 }
