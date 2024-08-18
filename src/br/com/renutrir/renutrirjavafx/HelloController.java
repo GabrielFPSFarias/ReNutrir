35cfb15bd2c0338e654d73f520b81ed8a788607c
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-18 19:19:20 -0300

    Create HelloController.java

---
+++ src/br/com/renutrir/renutrirjavafx/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.renutrir.renutrirjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
