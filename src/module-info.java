a3c08d2de57c9ced298dfeb73f2c3334c7511b24
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-18 20:02:18 -0300

    Delete src/module-info.java

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module br.com.renutrir.renutrirjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens br.com.renutrir.renutrirjavafx to javafx.fxml;
-    exports br.com.renutrir.renutrirjavafx;
-}
