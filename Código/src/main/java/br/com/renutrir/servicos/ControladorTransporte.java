f8947bb13e01a3929755e38c486aa7da328c7e23
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-08-19 11:53:43 -0300

    Update ControladorTransporte.java

--- Código/src/main/java/br/com/renutrir/servicos/ControladorTransporte.java
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorTransporte.java
@@ -1,10 +1,12 @@
 public class ControladorTransporte{
 
-  private tipoTransporte;
-  private tipoDoacao;]
-  limite de expansão do raio para transporteVoluntario
-  tipoDoacao
-  Horario de entrega
-  Horarioo de buscar
+  public void localizarInstituição{
+
+    limite de expansão do raio para transporteVoluntario
+  Depois avalia o tipoDoacao
+  }
+  public void avaliarHorarioDeDoacao{
+  }
+  
 
 }
