d4179fd4097372a4144fe703d430c84a36fab9b3
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-20 07:47:04 -0300

    Update RepositorioDoacoes.java

--- Código/src/main/java/br/com/renutrir/repositorio/RepositorioDoacoes.java
+++ Código/src/main/java/br/com/renutrir/repositorio/RepositorioDoacoes.java
@@ -1,6 +1,6 @@
 package br.com.renutrir.repositorio;
 
-import br.com.renutrir.model.*;
+import br.com.renutrir.model.Doacao;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +13,38 @@ public class RepositorioDoacoes {
         this.doacoes = new ArrayList<>();
     }
 
+    // Adiciona uma nova doação ao repositório
     public void adicionarDoacao(Doacao doacao) {
         doacoes.add(doacao);
     }
 
-    public void removerDoacao(Doacao doacao) {
-        doacoes.remove(doacao);
+    // Remove uma doação do repositório pelo ID
+    public boolean removerDoacao(Doacao doacao) {
+        return doacoes.remove(doacao);
     }
 
-    //Listar todas as doações feitas
+    // Atualiza uma doação existente
+    public void atualizarDoacao(Doacao doacao) {
+        for (int i = 0; i < doacoes.size(); i++) {
+            if (doacoes.get(i).getId().equals(doacao.getId())) {
+                doacoes.set(i, doacao);
+                break;
+            }
+        }
+    }
+
+    // Busca uma doação pelo ID
+    public Doacao buscarDoacaoPorId(String id) {
+        for (Doacao doacao : doacoes) {
+            if (doacao.getId().equals(id)) {
+                return doacao;
+            }
+        }
+        return null;
+    }
+
+    // Retorna todas as doações
     public List<Doacao> listarDoacoes() {
-        return new ArrayList<>(doacoes);
+        return new ArrayList<>(doacoes); // Retorna uma cópia da lista
     }
 }
