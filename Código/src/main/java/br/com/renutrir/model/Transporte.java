b7b35de6491bb13cf55b68c9f40fc55a9f07a534
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-20 07:48:26 -0300

    Update Transporte.java

--- Código/src/main/java/br/com/renutrir/model/Transporte.java
+++ Código/src/main/java/br/com/renutrir/model/Transporte.java
@@ -1,9 +1,14 @@
+package br.com.renutrir.model;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 public class Transporte {
 
   private String tipoDeTransporte;
   
   //Herdar tipoDeDoacao da intenção de doação
   
-  private DateTime horarioDeColeta;
-  private DateTime horarioDeEntrega;
+  private LocalDateTime horarioDeColeta;
+  private LocalDateTime horarioDeEntrega;
 }
