b6aec69f23e2bf9444512e2408fd7849f283123e
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-20 08:05:56 -0300

    Update Doacao.java

--- Código/src/main/java/br/com/renutrir/model/Doacao.java
+++ Código/src/main/java/br/com/renutrir/model/Doacao.java
@@ -3,49 +3,66 @@ package br.com.renutrir.model;
 import java.time.LocalDate;
 
 public class Doacao {
-    private String tipo;
+    private String id;
+    private String descricao;
+    private int quantidade;
+    private String doadorId;
+    private String instituicaoId;
     private Doador doador;
     private Instituicao instituicao;
     private LocalDate data;
 
-    private int qtd;
-
-    public Doacao(String tipo, Doador doador, int qtd, Instituicao instituicao){
-    setTipo(tipo);
-    setDoador(doador);
-    setQtd(qtd);
-    setInstituicao(instituicao);
-    }
-    public int getQtd() {
-        return qtd;
-    }
-
-    public void setQtd(int qtd) {
-        this.qtd = qtd;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public Doador getDoador() {
-        return doador;
-    }
-
-    public void setDoador(Doador doador) {
+    public Doacao(String id, String descricao, int quantidade, String doadorId, String instituicaoId, Doador doador, Instituicao instituicao, LocalDate data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.doadorId = doadorId;
+        this.instituicaoId = instituicaoId;
         this.doador = doador;
-    }
-
-    public Instituicao getInstituicao() {
-        return instituicao;
-    }
-
-    public void setInstituicao(Instituicao instituicao) {
         this.instituicao = instituicao;
+        this.data = data;
+    }
+
+    // Getters e Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDoadorId() {
+        return doadorId;
+    }
+
+    public void setDoadorId(String doadorId) {
+        this.doadorId = doadorId;
+    }
+
+    public String getInstituicaoId() {
+        return instituicaoId;
+    }
+
+    public void setInstituicaoId(String instituicaoId) {
+        this.instituicaoId = instituicaoId;
     }
 
     public LocalDate getData() {
