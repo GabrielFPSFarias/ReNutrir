5088711dd0ba7b4080db4afb6de67820c931ade8
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 06:35:59 -0300

    Update Certificado.java

--- Código/src/main/java/br/com/renutrir/model/Certificado.java
+++ Código/src/main/java/br/com/renutrir/model/Certificado.java
@@ -6,38 +6,64 @@ public class Certificado {
     private String descricao;
     private LocalDate dataEmissao;
     private int quantDoacoes;
+    private static final int DOACOES_NECESSARIAS = 50;
 
+    // Construtor geral
     public Certificado(String descricao, LocalDate dataEmissao, int quantDoacoes) {
         this.descricao = descricao;
         this.dataEmissao = dataEmissao;
         this.quantDoacoes = quantDoacoes;
     }
 
+    // Construtor padrão
     public Certificado() {
+        this.quantDoacoes = 0;
     }
 
+    // Getters e Setters
     public int getQuantDoacoes() {
         return quantDoacoes;
     }
 
     public void setQuantDoacoes(int quantDoacoes) {
         this.quantDoacoes = quantDoacoes;
+        verificarEConcederCertificado();
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-     public LocalDate getDataEmissao() {
-        return dataEmissao;
-    }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public LocalDate getDataEmissao() {
+        return dataEmissao;
     }
 
     public void setDataEmissao(LocalDate dataEmissao) {
         this.dataEmissao = dataEmissao;
     }
 
+    private void verificarEConcederCertificado() {
+        if (this.quantDoacoes >= DOACOES_NECESSARIAS) {
+            emitirCertificado();
+        }
+    }
+
+    private void emitirCertificado() {
+        this.descricao = "Certificado concedido por atingir " + DOACOES_NECESSARIAS + " doações.";
+        this.dataEmissao = LocalDate.now();
+        System.out.println("Certificado emitido: " + this.descricao + " em " + this.dataEmissao);
+    }
+
+    public void adicionarDoacao() {
+        this.quantDoacoes++;
+        verificarEConcederCertificado();
+    }
+
+    public boolean isCertificadoEmitido() {
+        return this.dataEmissao != null;
+    }
 }
