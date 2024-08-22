b85b06de851780c70e894bcc8396e27098137b39
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 06:54:33 -0300

    Update HelloController.java

--- Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
+++ Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
@@ -1,14 +1,22 @@
-package br.com.renutrir.javafx;
+package br.com.renutrir.renutrir;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label textoTeste;
+
+    @FXML
+    public void botaoLogin() {
+        textoTeste.setText("Clicou no botão login");
+        System.out.println("Login");
+    }
+
+    @FXML
+    public void botaoCadastro() {
+        textoTeste.setText("Clicou no botão cadastro");
+        System.out.println("Cadastro");
     }
 }
