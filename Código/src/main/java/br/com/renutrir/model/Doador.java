013bfb79d8454ef0a0c3f61fdb7ca0f035f544ef
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 06:39:06 -0300

    Update Doador.java

--- Código/src/main/java/br/com/renutrir/model/Doador.java
+++ Código/src/main/java/br/com/renutrir/model/Doador.java
@@ -14,11 +14,6 @@ public class Doador extends Conta {
 
     // Construtor, getters e setters
 
-    public Doacao doar(String tipo, int qtd, Instituicao instituicao) {
-        Doacao doacao = new Doacao(tipo, this, qtd, instituicao);
-        return doacao;
-    }
-
     public void setNivel(int nivel){
         this.nivel = nivel;
     }
@@ -28,10 +23,10 @@ public class Doador extends Conta {
     }
 
     public void setDataNascimento(LocalDate dataNascimento) {
-            if (dataNascimento.isAfter(LocalDate.now())) {
-                throw new IllegalArgumentException("A data de nascimento não pode ser no futuro.");
-            }
-            this.dataNascimento = dataNascimento;
+        if (dataNascimento.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("A data de nascimento não pode ser no futuro.");
+        }
+        this.dataNascimento = dataNascimento;
     }
 
     public int getNivel(){
