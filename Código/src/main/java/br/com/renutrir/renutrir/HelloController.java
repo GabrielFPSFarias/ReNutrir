4586e037dab9734997c2064a1e2b6284904e1265
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 07:37:55 -0300

    Update HelloController.java

--- Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
+++ Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
@@ -1,7 +1,13 @@
 package br.com.renutrir.renutrir;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
 
@@ -9,14 +15,54 @@ public class HelloController {
     private Label textoTeste;
 
     @FXML
+    private Button botaoCadastro;
+
+    @FXML
+    private Button botaoLogin;
+
+    @FXML
     public void botaoLogin() {
         textoTeste.setText("Clicou no botão login");
         System.out.println("Login");
+        terceiraTelaLogin();
     }
 
     @FXML
     public void botaoCadastro() {
         textoTeste.setText("Clicou no botão cadastro");
         System.out.println("Cadastro");
+        segundaTelaCadastro();
+    }
+
+    @FXML
+    private void initialize() {
+    }
+
+    private void terceiraTelaLogin() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("03-login.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 800, 500));
+            stage.setTitle("ReNutrir - Login");
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void segundaTelaCadastro() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("02-pre-cadastro.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 800, 500));
+            stage.setTitle("ReNutrir - Cadastro");
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
