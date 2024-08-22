9478e97bc6738bfc61d2c9ae4af098794e37cd78
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 06:50:26 -0300

    Update Voluntario.java

--- Código/src/main/java/br/com/renutrir/model/Voluntario.java
+++ Código/src/main/java/br/com/renutrir/model/Voluntario.java
@@ -1,10 +1,23 @@
 package br.com.renutrir.model;
 
-public class Voluntario extends Doador{
+import java.util.Objects;
+
+public class Voluntario extends Doador {
+
     private String disponibilidade; // Ex: dias e horários disponíveis
     private String funcaoVoluntariado; // o cargo que o voluntário vai cumprir
-    private String instituicaoVinculada; // refere-se à instituição a que o voluntário está prestando serviço.
+    private String instituicaoVinculada; // refere-se à instituição a que o voluntário está prestando serviço
+
+    public Voluntario() {
+    }
     
+    public Voluntario(String nome, String email, String telefone, String disponibilidade,
+                      String funcaoVoluntariado, String instituicaoVinculada) {
+        super(nome, email, telefone);
+        this.disponibilidade = disponibilidade;
+        this.funcaoVoluntariado = funcaoVoluntariado;
+        this.instituicaoVinculada = instituicaoVinculada;
+    }
 
     public String getDisponibilidade() {
         return disponibilidade;
@@ -29,4 +42,43 @@ public class Voluntario extends Doador{
     public void setInstituicaoVinculada(String instituicaoVinculada) {
         this.instituicaoVinculada = instituicaoVinculada;
     }
+
+    // Verificar a disponibilidade
+    public boolean estaDisponivel(String dia) {
+        return disponibilidade != null && disponibilidade.contains(dia);
+    }
+
+    // Verificar se o voluntário está vinculado
+    public boolean estaVinculadoA(String instituicao) {
+        return instituicaoVinculada != null && instituicaoVinculada.equalsIgnoreCase(instituicao);
+    }
+
+    @Override
+    public String toString() {
+        return "Voluntario{" +
+                "nome='" + getNome() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", telefone='" + getTelefone() + '\'' +
+                ", disponibilidade='" + disponibilidade + '\'' +
+                ", funcaoVoluntariado='" + funcaoVoluntariado + '\'' +
+                ", instituicaoVinculada='" + instituicaoVinculada + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Voluntario)) return false;
+        if (!super.equals(o)) return false;
+        Voluntario that = (Voluntario) o;
+        return Objects.equals(disponibilidade, that.disponibilidade) &&
+                Objects.equals(funcaoVoluntariado, that.funcaoVoluntariado) &&
+                Objects.equals(instituicaoVinculada, that.instituicaoVinculada);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), disponibilidade, funcaoVoluntariado, instituicaoVinculada);
+    }
 }
+
