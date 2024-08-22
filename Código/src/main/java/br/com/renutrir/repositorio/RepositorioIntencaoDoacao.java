06d47f36c1b3d15b2db7944a511fdff7d5055b35
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-22 06:57:37 -0300

    Update RepositorioIntencaoDoacao.java

--- Código/src/main/java/br/com/renutrir/repositorio/RepositorioIntencaoDoacao.java
+++ Código/src/main/java/br/com/renutrir/repositorio/RepositorioIntencaoDoacao.java
@@ -17,34 +17,3 @@ public class RepositorioIntencaoDoacao {
     public void adicionarIntencao(IntencaoDoacao intencao) {
         intencoes.add(intencao);
     }
-
-    // Remove uma intenção de doação do repositório pelo ID
-    public boolean removerIntencao(IntencaoDoacao intencao) {
-        return intencoes.remove(intencao);
-    }
-
-    // Atualiza uma intenção de doação existente
-    public void atualizarIntencao(IntencaoDoacao intencao) {
-        for (int i = 0; i < intencoes.size(); i++) {
-            if (intencoes.get(i).getId().equals(intencao.getId())) {
-                intencoes.set(i, intencao);
-                break;
-            }
-        }
-    }
-
-    // Busca uma intenção de doação pelo ID
-    public IntencaoDoacao buscarIntencaoPorId(String id) {
-        for (IntencaoDoacao intencao : intencoes) {
-            if (intencao.getId().equals(id)) {
-                return intencao;
-            }
-        }
-        return null;
-    }
-
-    // Retorna todas as intenções de doação
-    public List<IntencaoDoacao> listarIntencoes() {
-        return new ArrayList<>(intencoes); // Retorna uma cópia da lista
-    }
-}
