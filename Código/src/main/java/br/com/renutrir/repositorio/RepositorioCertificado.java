0b56cab79dc4c4e078cef9b1799dfbc18dd08ec4
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-08-26 09:59:29 -0300

    Update RepositorioCertificado.java

--- Código/src/main/java/br/com/renutrir/repositorio/RepositorioCertificado.java
+++ Código/src/main/java/br/com/renutrir/repositorio/RepositorioCertificado.java
@@ -20,3 +20,36 @@ public class RepositorioCertificado {
         return new ArrayList<>(certificados);
     }
 }
+/*
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepositorioDeCertificados {
+    private Map<String, Certificado> certificados;
+
+    public RepositorioDeCertificados() {
+        this.certificados = new HashMap<>();
+    }
+
+    public void adicionarCertificado(Certificado certificado) {
+        certificados.put(certificado.getDoador().getNome(), certificado);
+    }
+
+    public Certificado obterCertificado(String nomeDoador) {
+        return certificados.get(nomeDoador);
+    }
+
+    public void removerCertificado(String nomeDoador) {
+        certificados.remove(nomeDoador);
+    }
+
+    public void listarCertificados() {
+        for (Certificado certificado : certificados.values()) {
+            System.out.println(certificado);
+        }
+    }
+
+    public boolean verificarCertificado(String nomeDoador) {
+        return certificados.containsKey(nomeDoador);
+    }
+}*/
