6f6aa6f70e86f507447ab4d66ff7fa144ad88019
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-27 06:00:12 -0300

    Update Transacao.java

--- Código/src/main/java/br/com/renutrir/model/Transacao.java
+++ Código/src/main/java/br/com/renutrir/model/Transacao.java
@@ -1,9 +1,54 @@
 package br.com.renutrir.model;
 
-public class Transacao{
+public class Transacao {
 
     private double valorTransacao;
     private String idTransacao;
-    //Doador e instituição
+    private Doador doador;
+    private Instituicao instituicao;
 
+    //Construtor com todos os parâmetros
+    public Transacao(double valorTransacao, String idTransacao, Doador doador, Instituicao instituicao) {
+        this.valorTransacao = valorTransacao;
+        this.idTransacao = idTransacao;
+        this.doador = doador;
+        this.instituicao = instituicao;
+    }
+
+    public double getValor() {
+        return this.valorTransacao;
+    }
+
+    // Getters e Setters
+    public double getValorTransacao() {
+        return valorTransacao;
+    }
+
+    public void setValorTransacao(double valorTransacao) {
+        this.valorTransacao = valorTransacao;
+    }
+
+    public String getIdTransacao() {
+        return idTransacao;
+    }
+
+    public void setIdTransacao(String idTransacao) {
+        this.idTransacao = idTransacao;
+    }
+
+    public Doador getDoador() {
+        return doador;
+    }
+
+    public void setDoador(Doador doador) {
+        this.doador = doador;
+    }
+
+    public Instituicao getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(Instituicao instituicao) {
+        this.instituicao = instituicao;
+    }
 }
