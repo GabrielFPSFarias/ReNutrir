e56cb0644c4369020f5dd0c4f11709f92a30155b
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-08-27 05:57:32 -0300

    Update HelloApplication.java

--- Código/src/main/java/br/com/renutrir/renutrir/HelloApplication.java
+++ Código/src/main/java/br/com/renutrir/renutrir/HelloApplication.java
@@ -6,13 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("01-tela-inicial.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/br/com/renutrir/01-tela-inicial.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("ReNutrir");
         stage.setScene(scene);
