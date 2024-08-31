f273c6ecdd1979dbaef1eeab9e61763b63b498ab
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-08-31 18:00:20 -0300

    Update and rename ControladorTransacaoPix to ControladorTransacaoPix.java

---
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorTransacaoPix.java
@@ -0,0 +1,42 @@
+public class TransacaoPix {
+// Limite mínimo e máximo para valor de transação, exemplo fictício.
+      private static final double VALOR_MINIMO = 0.01;
+      private static final double VALOR_MAXIMO = 1000000000.00;
+  
+      public boolean validarTransacao(Transacao transacao) {
+          return validarIdTransacao(transacao.getIdTransacao()) &&
+                 validarValor(transacao.getValor());
+      }
+  
+      public static boolean validarIdTransacao(String idTransacao) {
+        // Verifica se o ID é nulo ou não possui 32 caracteres
+        if (idTransacao == null || idTransacao.length() != 32) {
+            return false;
+        }
+        
+        // Verifica se o ID contém apenas caracteres alfanuméricos
+        if (!idTransacao.matches("[A-Za-z0-9]+")) {
+            return false;
+        }
+
+      if(identificarIdsDeTransacao(ArrayList idsDeTransacao, String idDeTransacaoInserido)){
+
+        //Escrever que esse ID de transação já foi utilizado pelo usuário
+
+        return false;
+
+      }
+        
+        // Se passou todas as verificações, o ID é válido
+        return true;
+     }
+
+      private boolean validarValor(double valor) {
+          if (valor < VALOR_MINIMO || valor > VALOR_MAXIMO) {
+              System.out.println("Valor da transação deve estar entre " + VALOR_MINIMO + " e " + VALOR_MAXIMO + ".");
+              return false;
+          }
+          return true;
+      }
+
+}
