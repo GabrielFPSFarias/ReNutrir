625a0826245aa4407ec5a46b65c365d02f025ceb
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-09-01 13:22:02 -0300

    Update TransacaoCartaoCredito.java

--- Código/src/main/java/br/com/renutrir/model/TransacaoCartaoCredito.java
+++ Código/src/main/java/br/com/renutrir/model/TransacaoCartaoCredito.java
@@ -2,5 +2,44 @@ package br.com.renutrir.model;
 
 public class TransacaoCartaoCredito extends Transacao {
 
-  
+  String numeroCartao;
+  String dataValidade;
+  String nomeTitular;
+  String cvv;
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    // Getter e Setter para dataValidade
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    // Getter e Setter para nomeTitular
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    // Getter e Setter para cvv
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
 }
+
