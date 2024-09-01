c2ed86713647f8bc1c5faa32df124a4ce586ea5b
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-09-01 14:14:52 -0300

    Update ControladorTransacaoPix.java

--- Código/src/main/java/br/com/renutrir/servicos/ControladorTransacaoPix.java
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorTransacaoPix.java
@@ -1,24 +1,21 @@
 package br.com.renutrir.servicos;
 
+import br.com.renutrir.model.TransacaoPix;
 import java.util.ArrayList;
-
-import br.com.renutrir.model.Doador;
-import br.com.renutrir.model.Transacao;
 
 public class ControladorTransacaoPix {
 
-    // Limite mínimo e máximo para valor de transação, exemplo fictício.
-    private static final double VALOR_MINIMO = 0.01;
-    private static final double VALOR_MAXIMO = 1000000000.00;
 
-    public boolean validarTransacaoPix(Transacao transacao) {
-        return validarIdTransacao(transacao) &&
-                validarValor(transacao.getValorTransacao());
+// Método para validar uma transação Pix
+    public boolean validarTransacaoPix(TransacaoPix transacaoPix) {
+        return validarIdTransacao(transacaoPix) &&
+               validarValor(transacaoPix.getValorTransacao());
     }
 
-    private boolean validarIdTransacao(Transacao transacao) {
-        String idTransacao = transacao.getIdTransacao();
-        Doador doador = transacao.getDoador();
+    // Método para validar o ID da transação Pix
+    private boolean validarIdTransacao(TransacaoPix transacaoPix) {
+        String idTransacao = transacaoPix.getIdTransacao();
+        ArrayList<String> idsDeTransacao = transacaoPix.getDoador().getIdsDeTransacao();
 
         // Verifica se o ID é nulo ou não possui 32 caracteres
         if (idTransacao == null || idTransacao.length() != 32) {
@@ -31,7 +28,7 @@ public class ControladorTransacaoPix {
         }
 
         // Verifica se o ID já foi utilizado pelo doador
-        if (identificarIdsDeTransacao(doador.getIdsDeTransacao(), idTransacao)) {
+        if (identificarIdsDeTransacao(idsDeTransacao, idTransacao)) {
             System.out.println("Esse ID de transação já foi utilizado pelo usuário.");
             return false;
         }
@@ -40,18 +37,14 @@ public class ControladorTransacaoPix {
         return true;
     }
 
+    // Método para identificar se o ID de transação já foi usado
     private boolean identificarIdsDeTransacao(ArrayList<String> idsDeTransacao, String idDeTransacaoInserido) {
         // Verifica se o ID está na lista de IDs do Doador
         return idsDeTransacao.contains(idDeTransacaoInserido);
     }
+}
 
-    private boolean validarValor(double valor) {
-        if (valor < VALOR_MINIMO || valor > VALOR_MAXIMO) {
-            System.out.println("Valor da transação deve estar entre " + VALOR_MINIMO + " e " + VALOR_MAXIMO + ".");
-            return false;
-        }
-        return true;
-    }
+   
 
 //Abaixo está o código anterior
 
