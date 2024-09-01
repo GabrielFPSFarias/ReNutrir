00592d47148ddc7e428f137cc2145a513753599a
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-09-01 13:30:13 -0300

    Update TransacaoPix.java

--- Código/src/main/java/br/com/renutrir/model/TransacaoPix.java
+++ Código/src/main/java/br/com/renutrir/model/TransacaoPix.java
@@ -4,4 +4,22 @@ public class TransacaoPix extends Transacao{
 
     private double valorTransacao;
     private String idTransacao;
+
+     public double getValorTransacao() {
+        return valorTransacao;
+    }
+
+    public void setValorTransacao(double valorTransacao) {
+        this.valorTransacao = valorTransacao;
+    }
+
+    // Getter e Setter para idTransacao
+    public String getIdTransacao() {
+        return idTransacao;
+    }
+
+    public void setIdTransacao(String idTransacao) {
+        this.idTransacao = idTransacao;
+    }
+
 }
