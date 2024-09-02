06de1ecb804ec63bf9bca486eef653aefefff450
GabrielFPSFarias <gabriel.fpsfarias@ufrpe.br>
2024-09-02 07:47:12 -0300

    Create RepositorioEventos.java

---
+++ Código/src/main/java/br/com/renutrir/repositorio/RepositorioEventos.java
@@ -0,0 +1,50 @@
+package br.com.renutrir.repositorio;
+
+import br.com.renutrir.model.Evento;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioEventos {
+    private List<Evento> eventos;
+
+    public RepositorioEventos() {
+        this.eventos = new ArrayList<>();
+    }
+
+    // Adiciona um novo evento ao repositório
+    public void adicionarEvento(Evento evento) {
+        eventos.add(evento);
+    }
+
+    // Remove um evento do repositório
+    public boolean removerEvento(Evento evento) {
+        return eventos.remove(evento);
+    }
+
+    // Remove eventos passados
+    public void atualizarEventos() {
+        for (int i = 0; i < eventos.size(); i++) {
+            if (eventos.get(i).getData().isAfter(LocalDate.now())){
+                eventos.remove(eventos.get(i));
+            }
+        }
+    }
+
+    // Busca um evento
+    public Evento buscarEvento(Evento e) {
+        for (Evento evento : eventos) {
+            if (evento.equals(e)) {
+                return evento;
+            }
+        }
+        return null;
+    }
+
+    // retorna todas as doações
+    public List<Evento> listarEventos() {
+        return new ArrayList<>(eventos); // Retorna uma cópia da lista
+    }
+}
