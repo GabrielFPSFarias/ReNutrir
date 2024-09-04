e00db11473fd97f66a8a56cafd66eabe46f497c8
Daniel DDO <dandionisio70@gmail.com>
2024-09-04 11:35:29 -0300

    Update ControladorIntencaoDeDoacao.java

--- Código/src/main/java/br/com/renutrir/servicos/ControladorIntencaoDeDoacao.java
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorIntencaoDeDoacao.java
@@ -1,47 +1,47 @@
 package br.com.renutrir.servicos;
 
+import br.com.renutrir.model.IntencaoDoacao;
+import br.com.renutrir.repositorio.RepositorioIntencaoDoacao;
+import javafx.fxml.FXML;
+import br.com.renutrir.model.Endereco;
 import br.com.renutrir.model.Instituicao;
-import br.com.renutrir.renutrir.HelloController;
-import br.com.renutrir.repositorio.RepositorioInstituicao;
+import br.com.renutrir.repositorio.*;
+import br.com.renutrir.model.Doador;
+import br.com.renutrir.servicos.*;
+import br.com.renutrir.main.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.util.Optional;
+import java.awt.*;
 import java.util.List;
+import br.com.renutrir.renutrir.HelloController;
+import br.com.renutrir.sessao.SessaoDoador;
+import br.com.renutrir.sessao.SessaoInstituicao;
 
 public class ControladorIntencaoDeDoacao {
 
     private HelloController hc;
-
-    @FXML
-    private ComboBox<String> escolherInstituicaoDoarCbox;
-
-    private RepositorioInstituicao repositorioInstituicao;
-
-    @FXML
-    public void initialize() {
-        repositorioInstituicao = new RepositorioInstituicao();
-        cboxEscolherInstituicaoDoar();
-    }
-
-    @FXML
-    void cboxEscolherInstituicaoDoar() {
-        List<Instituicao> instituicoes = repositorioInstituicao.listarInstituicoes();
-        for (Instituicao instituicao : instituicoes) {
-            escolherInstituicaoDoarCbox.getItems().add(instituicao.getNome());
-        }
-    }
-
-    @FXML
-    public void botaoInstituicoesDoacao(ActionEvent actionEvent) {
-        hc.realizarTrocaDeTela("/br/com/renutrir/06-doacoes-solicitadas.fxml", "ReNutrir - Doações Solicitadas");
-    }
-
-    /*
     private RepositorioIntencaoDoacao repositorioIntencaoDoacao;
 
-    public ControladorIntencaoDeDoacao() {
+    public ControladorIntencaoDeDoacao(HelloController hc) {
         this.repositorioIntencaoDoacao = new RepositorioIntencaoDoacao();
+        this.hc = hc;
+    }
+
+    public List<IntencaoDoacao> listarIntencoes() {
+        return repositorioIntencaoDoacao.listarIntencoes();
     }
 
     public void criarIntencaoDeDoacao(IntencaoDoacao intencao) {
@@ -53,9 +53,6 @@ public class ControladorIntencaoDeDoacao {
         }
     }
 
-    public List<IntencaoDoacao> listarIntencoes() {
-        return repositorioIntencaoDoacao.listarIntencoes();
-    }
 
     public void atualizarIntencaoDeDoacao(IntencaoDoacao intencao) {
         if (validarIntencaoDeDoacao(intencao)) {
@@ -84,6 +81,6 @@ public class ControladorIntencaoDeDoacao {
 
         return true;
     }
-    */
+
 
 }
