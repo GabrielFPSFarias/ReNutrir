59e8a6795e9f025b0c9d13fc02af70c9391ba96c
Daniel DDO <dandionisio70@gmail.com>
2024-09-07 10:40:29 -0300

    Update main

--- Código/src/main/java/br/com/renutrir/main/Main.java
+++ Código/src/main/java/br/com/renutrir/main/Main.java
@@ -1,17 +1,9 @@
 package br.com.renutrir.main;
 
-import br.com.renutrir.model.*;
-import br.com.renutrir.repositorio.*;
-import br.com.renutrir.servicos.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import br.com.renutrir.renutrir.HelloApplication;
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		HelloApplication.main(args);
 	}
 }
