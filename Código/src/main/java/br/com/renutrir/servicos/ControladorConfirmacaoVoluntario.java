312bee1c2f39b256a1c43fd961836c6827e40616
Alvin-RBS <alvaro.ribeirobs@outlook.com>
2024-09-12 09:22:08 -0300

    Merge remote-tracking branch 'origin/main'

--- Código/src/main/java/br/com/renutrir/servicos/ControladorConfirmacaoVoluntario.java
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorConfirmacaoVoluntario.java
@@ -1,60 +1,153 @@
 package br.com.renutrir.servicos;
 
+import br.com.renutrir.model.Doador;
 import br.com.renutrir.model.Instituicao;
+import br.com.renutrir.model.IntencaoDoacao;
+import br.com.renutrir.repositorio.RepositorioIntencaoDoacao;
+import br.com.renutrir.sessao.SessaoDoador;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import br.com.renutrir.repositorio.RepositorioInstituicao;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import br.com.renutrir.repositorio.RepositorioVoluntario;
 
 public class ControladorConfirmacaoVoluntario {
 
-    private  ControladorEscolhaInstituicaoFuncaoVoluntario controladorEscolhaInstituicaoFuncaoVoluntario;
+    private ControladorEscolhaInstituicaoFuncaoVoluntario controladorEscolha;
     private RepositorioInstituicao repositorioInstituicoes;
 
     public ControladorConfirmacaoVoluntario() {
 
-       this.controladorEscolhaInstituicaoFuncaoVoluntario = new ControladorEscolhaInstituicaoFuncaoVoluntario();
-       this.repositorioInstituicoes = new RepositorioInstituicao();
-    }
-@FXML
- private Label instituicaoInformacoesLabel;
-
-@FXML
-Button voltarBotao72;
-@FXML
-private Button confirmarVoluntarioBotao;
-@FXML
-public void voltarBotao72(){
-
-    controladorEscolhaInstituicaoFuncaoVoluntario.realizarTrocaDeTela("/br/com/renutrir/27-escolha-instituicao-funcao-voluntario.fxml", "ReNutrir - Escolha de instituição e função do voluntário",voltarBotao72);
+        this.controladorEscolha = new ControladorEscolhaInstituicaoFuncaoVoluntario();
+        this.repositorioInstituicoes = new RepositorioInstituicao();
     }
 
-    public String getConfirmacaoDados (String nomeUsuario) {
+    @FXML
+    private Label instituicaoInformacoesLabel;
 
-        Optional<Instituicao> instituicaoOptional = repositorioInstituicoes.buscarInstituicaoPorNomeUsuario(nomeUsuario);
+    @FXML
+    Button voltarBotao72;
+    @FXML
+    private Button confirmarVoluntarioBotao;
 
-        if (instituicaoOptional.isPresent()) {
+    @FXML
+    public void voltarBotao72() {
 
-            Instituicao instituicao = instituicaoOptional.get();
+        controladorEscolha.realizarTrocaDeTela("/br/com/renutrir/27-escolha-instituicao-funcao-voluntario.fxml", "ReNutrir - Escolha de instituição e função do voluntário", voltarBotao72);
+    }
 
-            StringBuilder detalhes = new StringBuilder();
-            detalhes.append("Nome: ").append(instituicao.getNome()).append("\n\n");
-            detalhes.append(instituicao.getEndereco()).append("\n\n");
-            detalhes.append("Telefone: ").append(instituicao.getTelefone()).append("\n\n");
-            detalhes.append("Email: ").append(instituicao.getEmail()).append("\n\n");
-            detalhes.append("Horário de funcionamento: ").append(instituicao.getHorarioInicial()).append(" às ").append(instituicao.getHorarioFinal());
-            detalhes.append("Descrição: ").append(instituicao.getDescricao());
+    /*@FXML
+    void doarAlimentosBotao(ActionEvent actionEvent) {
+        Instituicao instituicaoSelecionada = RepositorioIntencaoDoacao.getInstituicaoSelecionada();
 
-            return detalhes.toString();
-        } else {
-            return "Detalhes da instituição não encontrados.";
+        if (instituicaoSelecionada == null) {
+            hc.showAlert(Alert.AlertType.ERROR, "Erro", "Nenhuma instituição selecionada");
+            return;
         }
+
+        System.out.println("Instituição selecionada: " + instituicaoSelecionada.getNome());
+
+        String nomeItem = fieldItemDoarIntencao.getText();
+        String qtdItem = fieldInserirQtdItem.getText();
+        IntencaoDoacao intencaoDoacao = new IntencaoDoacao(SessaoDoador.getInstancia().getDoadorLogado(), instituicaoSelecionada, Integer.parseInt(qtdItem), "Alimentos", nomeItem);
+        RepositorioIntencaoDoacao repositorioIntencaoDoacao = new RepositorioIntencaoDoacao();
+        repositorioIntencaoDoacao.adicionarIntencao(intencaoDoacao);
+
+        int quantidade;
+        try {
+            quantidade = Integer.parseInt(qtdItem);
+        } catch (NumberFormatException e) {
+            showAlert(Alert.AlertType.ERROR, "Erro de Validação", "A quantidade deve ser um número válido.");
+            return;
+        }
+
+        Doador doadorLogado = SessaoDoador.getInstancia().getDoadorLogado();
+        String doadorNome = doadorLogado != null ? doadorLogado.getNome() : "Desconhecido";
+        String tipoDoacao = "Alimentos";
+        LocalDateTime dataHora = LocalDateTime.now();
+
+        try {
+            URL fxmlUrl = getClass().getResource("/br/com/renutrir/07-9-intencao-concluida.fxml");
+            if (fxmlUrl == null) {
+                throw new IOException("FXML não encontrado.");
+            }
+
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            Parent root = loader.load();
+            ControladorIntencaoDeDoacao controlador = loader.getController();
+            controlador.setInformacoesDoacao(doadorNome, tipoDoacao, quantidade, nomeItem, dataHora, instituicaoSelecionada);
+            showAlert(Alert.AlertType.INFORMATION, "Intenção Realizada", "Sua intenção de doação foi realizada com sucesso!");
+
+            Stage stage = (Stage) botaoItemDoar.getScene().getWindow();
+            stage.setTitle("ReNutrir - Intenção Concluída");
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert(Alert.AlertType.ERROR, "Erro", "Não foi possível carregar a tela de doação concluída.");
+        }
+    }*/
+
+    // Método para receber a instituição e função e exibi-las
+
+
+    public void setConfirmacaoDados(Instituicao instituicao, String funcaoVoluntario) {
+
+
+        StringBuilder detalhes = new StringBuilder();
+        detalhes.append("Nome: ").append(instituicao.getNome()).append("\n\n");
+        detalhes.append(instituicao.getEndereco()).append("\n\n");
+        detalhes.append("Telefone: ").append(instituicao.getTelefone()).append("\n\n");
+        detalhes.append("Email: ").append(instituicao.getEmail()).append("\n\n");
+        detalhes.append("Horário de funcionamento: ").append(instituicao.getHorarioInicial()).append(" às ").append(instituicao.getHorarioFinal()).append("\n\n");
+        ;
+        detalhes.append("Função: ").append(funcaoVoluntario).append("\n\n");
+        ;
+
+        instituicaoInformacoesLabel.setText(detalhes.toString());
+
+        this.instituicaoSelecionada = instituicao;
+        this.funcaoVoluntarioSelecionada = funcaoVoluntario;
+
     }
-@FXML
-public void confirmarVoluntarioBotao (){
 
+    private Instituicao instituicaoSelecionada;
+    private String funcaoVoluntarioSelecionada;
 
+    @FXML
+    public void confirmarVoluntarioBotao() {
+        if (instituicaoSelecionada != null && funcaoVoluntarioSelecionada != null) {
+            // Supondo que você tenha um repositório para salvar os voluntários ou algo similar
+            RepositorioVoluntario repositorioVoluntario = new RepositorioVoluntario();
+
+            // Aqui pode ser criado um objeto de voluntário ou função do voluntário
+            repositorioVoluntario.adicionarFuncao(funcaoVoluntarioSelecionada);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Confirmação");
+            alert.setHeaderText(null);
+            alert.setContentText("Sua participação como voluntário foi confirmada!");
+            alert.showAndWait();
+
+            // Redirecionar para outra tela após a confirmação
+            controladorEscolha.realizarTrocaDeTela("/br/com/renutrir/08-menu-voluntario.fxml", "ReNutrir - Voluntariado Concluído", confirmarVoluntarioBotao);
+        } else {
+            // Caso nenhuma instituição ou função tenha sido selecionada, exibir alerta
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Atenção");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor, selecione uma instituição e uma função de voluntário.");
+            alert.showAndWait();
+        }
     }
 }
--- Código/src/main/java/br/com/renutrir/servicos/ControladorEscolhaInstituicaoFuncaoVoluntario.java
+++ Código/src/main/java/br/com/renutrir/servicos/ControladorEscolhaInstituicaoFuncaoVoluntario.java
@@ -2,18 +2,27 @@ package br.com.renutrir.servicos;
 
 import br.com.renutrir.model.Doador;
 import br.com.renutrir.model.Instituicao;
+import br.com.renutrir.model.IntencaoDoacao;
 import br.com.renutrir.repositorio.RepositorioInstituicao;
+import br.com.renutrir.repositorio.RepositorioIntencaoDoacao;
 import br.com.renutrir.repositorio.RepositorioVoluntario;
 import br.com.renutrir.sessao.SessaoDoador;
 import br.com.renutrir.sessao.SessaoInstituicao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import br.com.renutrir.renutrir.HelloController;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -30,6 +39,9 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
     }
 
     HelloController hc = new HelloController();
+
+    private Instituicao instituicaoSelecionada;
+    private String funcaoVoluntarioSelecionada;
 
     @FXML
     private ListView<Instituicao> instituicaoListView;
@@ -54,23 +66,60 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Preencher o ListView com Instituicoes
-        //instituicoesListView.setItems(FXCollections.observableArrayList(nomesInstituicoes));
-        //instituicoesListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-       // instituicoesListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) ->
-
+        // Carregar as instituições na ListView
         carregarListView();
 
-        if (instituicaoListView != null) {
-            carregarListView();
-        } else {
-            System.err.println("instituicoesListView é null!");
-        }
+        // Configurar a ListView de funções de voluntário
+        carregarFuncoesVoluntarios();
 
-        configurarComboBox();
+        // Adiciona listener para a ListView de Instituições
+        instituicaoListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                instituicaoSelecionada = newValue;
+                String detalhesInst = getDetalhes(newValue.getNomeUsuario());
+                instituicaoDetalhesLabel.setText(detalhesInst);
+            } else {
+                System.out.println("Nenhuma instituição selecionada");
+            }
+        });
 
+        // Adiciona listener para a ListView de Funções de Voluntário
+        funcaoVoluntarioListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                funcaoVoluntarioSelecionada = newValue;
+            } else {
+                System.out.println("Nenhuma função de voluntário selecionada");
+            }
+        });
+
+        // Listener para o campo de busca
         buscaTextField.textProperty().addListener((observable, oldValue, newValue) -> filtrarInstituicoes(newValue));
     }
+
+    public void salvarSelecao(){
+
+        instituicaoListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+
+                instituicaoSelecionada = newValue;
+
+            } else {
+                System.out.println("Nenhuma instituição selecionada");
+            }
+        });
+
+
+        funcaoVoluntarioListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                funcaoVoluntarioSelecionada = newValue;
+            } else {
+                funcaoVoluntarioSelecionada = "Transportador voluntário";
+            }
+        });
+    }
+
+    @FXML
+    private ListView<String> funcaoVoluntarioListView;
 
     public void carregarListView(){
 
@@ -106,7 +155,7 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
         });
          } else System.err.println("ListView é nulo");
     }
-
+    @FXML
     private void carregarFuncoesVoluntarios() {
         ObservableList<String> funcoesVoluntarios = FXCollections.observableArrayList();
 
@@ -116,22 +165,19 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
         funcoesVoluntarios.addAll(funcaoVoluntario);
 
         if (funcoesVoluntarios.isEmpty()) {
-            cboxFuncaoVoluntario.setItems(FXCollections.observableArrayList("Nenhuma função disponível"));
+            funcaoVoluntarioListView.setItems(FXCollections.observableArrayList("Nenhuma função disponível"));
         } else {
-            cboxFuncaoVoluntario.setItems(funcoesVoluntarios);
+            funcaoVoluntarioListView.setItems(funcoesVoluntarios);
         }
-        cboxFuncaoVoluntario.setValue(cboxFuncaoVoluntario.getItems().get(0));
     }
 
-    private void configurarComboBox() {
+    private void configurarListViewFuncoes() {
         if (cboxFuncaoVoluntario == null) {
-            System.out.println("cboxFuncaoVoluntario está null");
+            System.out.println("listFuncaoVoluntario está null");
             return;
         }
         carregarFuncoesVoluntarios();
     }
-
-
 
     public void realizarTrocaDeTela(String fxmlArquivo, String titulo, Button voltarBotao) {
         System.out.println("Clicou: " + fxmlArquivo);
@@ -156,7 +202,33 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
     @FXML
     public void proximoBotao1(){
 
-        realizarTrocaDeTela("/br/com/renutrir/28-confirmacao-voluntario.fxml", "ReNutrir - Confirmação do voluntário", proximoBotao1);
+        salvarSelecao();
+
+            if (instituicaoSelecionada != null && funcaoVoluntarioSelecionada != null) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/renutrir/28-confirmacao-voluntario.fxml"));
+                    Parent root = loader.load();
+
+                    ControladorConfirmacaoVoluntario controladorConfirmacao = loader.getController();
+
+                    controladorConfirmacao.setConfirmacaoDados(instituicaoSelecionada, funcaoVoluntarioSelecionada);
+
+                    Stage stage = (Stage) proximoBotao1.getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Atenção");
+                alert.setHeaderText(null);
+                alert.setContentText("Por favor, selecione uma instituição e uma função de voluntário.");
+                alert.showAndWait();
+            }
+
 
     }
 
@@ -180,7 +252,6 @@ public class ControladorEscolhaInstituicaoFuncaoVoluntario implements Initializa
             detalhes.append("Telefone: ").append(instituicao.getTelefone()).append("\n\n");
             detalhes.append("Email: ").append(instituicao.getEmail()).append("\n\n");
             detalhes.append("Horário de funcionamento: ").append(instituicao.getHorarioInicial()).append(" às ").append(instituicao.getHorarioFinal()).append("\n\n");
-            detalhes.append("Descrição: ").append((instituicao.getDescricao()));
 
 
             return detalhes.toString();
