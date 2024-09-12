8b26ecea7434ed89586b92f52b56c9a9aaa01648
Daniel DDO <dandionisio70@gmail.com>
2024-09-11 21:19:22 -0300

    HelloController update final

--- Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
+++ Código/src/main/java/br/com/renutrir/renutrir/HelloController.java
@@ -49,16 +49,6 @@ public class HelloController {
         return SessaoInstituicao.getInstancia().getInstituicaoLogada();
     }
 
-
-  /*  lv.getItems().addAll(FXCollections.observableArrayList(contas.getListaDeNomesInstituicoes()));
-
-     listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-        if (newValue != null) {
-            // Abrir a nova janela com os detalhes da instituição
-            DetalhesInstituicao.showDetalhes(newValue);
-        }
-    };*/
-
     @FXML
     public void botaoListaDoacaoEfetuada(){
 
@@ -160,8 +150,6 @@ public class HelloController {
         }
     }
 
-
-
     @FXML
     public void botaoVoltar() {
         realizarTrocaDeTela("/br/com/renutrir/01-tela-inicial.fxml", "ReNutrir");
@@ -195,16 +183,6 @@ public class HelloController {
     @FXML
     public void botaoVoltar6() {
         realizarTrocaDeTela("/br/com/renutrir/05-intencao-doacao.fxml", "ReNutrir - Intenção de Doação");
-    }
-
-    @FXML
-    public void botaoVoltar7() {
-        realizarTrocaDeTela("/br/com/renutrir/05-intencao-doacao.fxml", "ReNutrir - Doações Solicitadas");
-    }
-
-    @FXML
-    public void botaoVoltar8() {
-        realizarTrocaDeTela("/br/com/renutrir/04-menu-doador.fxml", "ReNutrir - Doador");
     }
 
     @FXML
@@ -243,11 +221,6 @@ public class HelloController {
     }
 
     @FXML
-    public void botaoVoltar16() {
-        realizarTrocaDeTela("/br/com/renutrir/04-menu-doador.fxml", "ReNutrir - Doador");
-    }
-
-    @FXML
     public void botaoVoltar17() {
         realizarTrocaDeTela("/br/com/renutrir/04-menu-doador.fxml", "ReNutrir - Doador");
     }
@@ -263,32 +236,12 @@ public class HelloController {
     }
 
     @FXML
-    public void botaoVoltar20() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Instituição");
-    }
-
-    @FXML
     public void botaoVoltar21() {
         realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Instituição");
     }
 
     @FXML
     public void botaoVoltar23() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Instituição");
-    }
-
-    @FXML
-    public void botaoVoltar24() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Instituição");
-    }
-
-    @FXML
-    public void botaoVoltar25() {
-        realizarTrocaDeTela("/br/com/renutrir/24-voluntarios.fxml", "ReNutrir - Voluntários");
-    }
-
-    @FXML
-    public void botaoVoltar26() {
         realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Instituição");
     }
 
@@ -360,63 +313,48 @@ public class HelloController {
     //Métodos do cadastro doador
 
     public void emailField() {
-
     }
 
     public void nomeField() {
-
     }
 
     public void cepField() {
-
     }
 
     public void cpfField() {
-
     }
 
     public void nomeUserField() {
-
     }
 
     public void senhaField() {
-
     }
 
     public void confSenhaField() {
-
     }
 
     public void telefoneField() {
-
     }
 
     public void enderecoField() {
-
     }
 
     public void refField() {
-
     }
 
     public void bairroField() {
-
     }
 
     public void numField() {
-
     }
 
     public void cidadeField() {
-
     }
 
     public void ufField() {
-
     }
 
     public void compField() {
-
     }
 
     //Tela de login (TEMPORÁRIO)
@@ -553,7 +491,7 @@ public class HelloController {
     public void emailInsField(ActionEvent actionEvent) {
     }
 
-//Botões mudança de tela
+    //Botões mudança de tela
 
     public void botaoItencaoDoacao(ActionEvent actionEvent) {
         realizarTrocaDeTela("/br/com/renutrir/05-intencao-doacao.fxml", "ReNutrir - Intenção de Doação");
@@ -593,27 +531,10 @@ public class HelloController {
     }
 
     public void instituicaoChecar(ActionEvent actionEvent) {
-        // Implementação necessária
     }
 
     public void botaoDoacoesSolicitadas(ActionEvent actionEvent) {
         realizarTrocaDeTela("/br/com/renutrir/07-confirmar-doacao.fxml", "ReNutrir - Confirmar Doação");
-    }
-
-    public void botaoSejaVoluntario(ActionEvent actionEvent) {
-        realizarTrocaDeTela("/br/com/renutrir/09-seja-voluntario.fxml", "ReNutrir - Seja Voluntario");
-    }
-
-    public void botaoTransportesDoacoes(ActionEvent actionEvent) {
-        realizarTrocaDeTela("/br/com/renutrir/10-transportes-doacoes.fxml", "ReNutrir - Transportes");
-    }
-
-    public void botaoTransportesPendentes(ActionEvent actionEvent) {
-        realizarTrocaDeTela("/br/com/renutrir/13-transportes-pendentes.fxml", "ReNutrir - Transportes Pendentes");
-    }
-
-    public void botaoTransportesConcluidos(ActionEvent actionEvent) {
-        realizarTrocaDeTela("/br/com/renutrir/14-transportes-concluidos.fxml", "ReNutrir - Transportes Concluidos");
     }
 
     public void botaoTransportesSolicitados(ActionEvent actionEvent) {
@@ -661,7 +582,6 @@ public class HelloController {
         realizarTrocaDeTela("/br/com/renutrir/25-solicitar-voluntarios.fxml", "ReNutrir - Solicitar Voluntários");
     }
 
-
     //Tela 15 Certificados e pontuação
 
     @FXML
@@ -682,7 +602,6 @@ public class HelloController {
 
         controladorCertificado.verificarProgressoParaCertificado(doador);
     }
-
 
     //Tela 17 Perfil doador
 
@@ -736,7 +655,6 @@ public class HelloController {
         }
     }
 
-
     //Tela 19 - Menu Instituição
 
     @FXML
@@ -744,29 +662,9 @@ public class HelloController {
         realizarTrocaDeTela("/br/com/renutrir/20-criar-eventos.fxml", "ReNutrir - Criar Evento");
     }
 
-
-    //Tela 20 - Criar eventos
-
     //Tela 22
 
     private ControladorSolicitacaoDoacao controladorSolicitacaoDoacao = new ControladorSolicitacaoDoacao();
-
-    @FXML
-    public void botaoVoltar40() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml", "ReNutrir - Menu Instituição");
-    }
-
-    @FXML
-    void solicitarDinheiro(ActionEvent event) {
-        controladorSolicitacaoDoacao.solicitarDinheiro(event);
-    }
-
-    @FXML
-    void solicitarAlimentos(ActionEvent event) {
-        controladorSolicitacaoDoacao.solicitarAlimentos(event);
-    }
-
-    //Tela 23 Perfil Instituição
 
     @FXML
     private Label exibirInfoInstLabel;
@@ -797,7 +695,9 @@ public class HelloController {
         }
     }
 
+    //Tela 24 Voluntários (inst)
 
+    public RepositorioVoluntario repositorioVoluntario = new RepositorioVoluntario();
 
     //Limpar a sessão
 
@@ -806,55 +706,4 @@ public class HelloController {
         doadorLogado = null;
     }
 
-    //Tela 24 Voluntários (inst)
-
-
-    @FXML
-    private Button criarFuncaorVoluntarioBotao;
-
-    @FXML
-    private TextField inserirFuncaoVoluntario;
-
-    public RepositorioVoluntario repositorioVoluntario = new RepositorioVoluntario();
-
-    @FXML
-    void botaoCriarFuncaoVoluntario(ActionEvent event) {
-        String funcao = inserirFuncaoVoluntario.getText();
-
-        if (funcao != null && !funcao.trim().isEmpty()) {
-            repositorioVoluntario.adicionarFuncao(funcao);
-            showAlert(Alert.AlertType.INFORMATION, "Função criada!", "Função de voluntário '" + funcao + "' foi adicionada com sucesso!");
-        } else {
-            showAlert(Alert.AlertType.ERROR, "Erro", "A função de voluntário não foi criada");
-        }
-    }
-
-
-    //Tela 25 Solicitar Voluntários
-
-    @FXML
-    void botaoVoltar58() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml","ReNutrir - Menu Instituição");
-    }
-
-
-    //Tela 26 Doações Pendentes
-
-    @FXML
-    void botaoVoltar57() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml","ReNutrir - Menu Instituição");
-    }
-
-    //Tela 26.1 Validar intenção
-
-    @FXML
-    void botaoVoltar59() {
-        realizarTrocaDeTela("/br/com/renutrir/19-menu-instituicao.fxml","ReNutrir - Menu Instituição");
-    }
-
-    public void cboxEscolherInstituicaoDoar(ActionEvent actionEvent) {
-    }
-
-
-    //Próximos métodos
 }
