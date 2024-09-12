3f54378eb22295547c90b161512d1f1b8e2dc020
Álvaro Ribeiro <123338523+Alvin-RBS@users.noreply.github.com>
2024-09-12 14:45:27 -0300

    Update RepositorioVoluntario.java

--- Código/src/main/java/br/com/renutrir/repositorio/RepositorioVoluntario.java
+++ Código/src/main/java/br/com/renutrir/repositorio/RepositorioVoluntario.java
@@ -1,8 +1,10 @@
 package br.com.renutrir.repositorio;
 
+import br.com.renutrir.model.Voluntario;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RepositorioVoluntario {
     private static final String CAMINHO_ARQUIVO = "src/dados/funcoesVoluntario.dat";
@@ -10,6 +12,7 @@ public class RepositorioVoluntario {
 
     public RepositorioVoluntario() {
         funcoesVoluntario = carregarFuncoesVoluntario();
+        voluntarios = carregarVoluntarios();
     }
 
     public List<String> carregarFuncoesVoluntario() {
@@ -54,4 +57,61 @@ public class RepositorioVoluntario {
         funcoesVoluntario.remove(funcao);
         salvarFuncoesVoluntario();
     }
+
+    private final String arquivoVoluntarios = "src/dados/voluntariosRegistrados.dat";
+    private List<Voluntario> voluntarios;
+
+
+
+    private List<Voluntario> carregarVoluntarios() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivoVoluntarios))) {
+            return (List<Voluntario>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+
+    private void salvarVoluntarios() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivoVoluntarios))) {
+            oos.writeObject(voluntarios);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void adicionarVoluntario(Voluntario voluntario) {
+        voluntarios.add(voluntario);
+        salvarVoluntarios();
+    }
+
+
+    public Optional<Voluntario> buscarVoluntarioNomeUsuario(String nomeUsuario) {
+        return voluntarios.stream()
+                .filter(v -> v.getNomeUsuario() != null && v.getCpf().equals(nomeUsuario))
+                .findFirst();
+    }
+
+    public void atualizarVoluntario(Voluntario voluntarioAtualizado) {
+        Optional<Voluntario> voluntarioExistente = buscarVoluntarioNomeUsuario(voluntarioAtualizado.getNomeUsuario());
+        if (voluntarioExistente.isPresent()) {
+            voluntarios.remove(voluntarioExistente.get());
+            voluntarios.add(voluntarioAtualizado);
+            salvarVoluntarios();
+        }
+    }
+
+    public void removerVoluntario(String nomeUsuario) {
+        voluntarios.removeIf(v -> v.getNomeUsuario() != null && v.getNomeUsuario().equals(nomeUsuario));
+        salvarVoluntarios();
+    }
+
+    public List<Voluntario> listarVoluntarios() {
+        return new ArrayList<>(voluntarios);
+    }
+
 }
